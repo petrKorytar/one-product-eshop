70d1d262a3bdc7f70e29c586bbe75c76c4510193
petrKorytar <petrkorytar@email.cz>
2022-01-11 21:53:13 +0100

    Height of the gallery
    Setting the maximum height of the gallery

--- src/components/BuyBtnAndDescription.js
+++ src/components/BuyBtnAndDescription.js
@@ -4,7 +4,16 @@ import './BuyBtnAndDescription.css'
 const BuyBtnAndDescription = () => {
     return (
         <div>
-            
+            <p>Buy Btn And Description</p>
+            <p>Buy Btn And Description</p>
+            <p>Buy Btn And Description</p>
+            <p>Buy Btn And Description</p>
+            <p>Buy Btn And Description</p>
+            <p>Buy Btn And Description</p>
+            <p>Buy Btn And Description</p>
+            <p>Buy Btn And Description</p>
+            <p>Buy Btn And Description</p>
+            <p>Buy Btn And Description</p>
         </div>
     )
 }
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,12 +1,10 @@
 import './Style/Home.css'
 import ShowImage from '../Gallery/imageGallery.js'
-import BuyBtnAndDescription from '../BuyBtnAndDescription.js'
 
 const MyHome = () => {
     return (  
         <>
-           <ShowImage/>
-           <BuyBtnAndDescription/>
+            <ShowImage/>
         </>
     )
 }
